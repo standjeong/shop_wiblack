541b6c5fc9956db8b79be50d4dd630f1fe9c9199
standjeong <andame9999@gmail.com>
2024-10-09 16:15:07 +0900

    chore : add Google Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        banner: ['Updock', 'cursive'],
+      },
       backgroundImage: {
         bannerGradient: 'linear-gradient(273deg, #f8ceec 0%, #a88beb 74%)',
       },
