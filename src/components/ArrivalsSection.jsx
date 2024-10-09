099be2e698d1bfc160e7b0a68bd747d56fe3db14
standjeong <andame9999@gmail.com>
2024-10-09 15:10:03 +0900

    style : responsive design to ArrivalsSection

--- src/components/ArrivalsSection.jsx
+++ src/components/ArrivalsSection.jsx
@@ -14,14 +14,14 @@ export default function ArrivalsSection({ title }) {
         {title} Arrivals
       </h2>
       {isLoading && <p>로딩중...</p>}
-      <ul className='grid grid-cols-4 gap-6'>
+      <ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 xl:gap-6 mx-6 xl:mx-4'>
         {products &&
           products
             .filter((product) => product.color === title)
             .splice(0, 4)
             .map((product) => (
               <li
-                className='flex items-end bg-[#3aac86] overflow-hidden rounded-[130px]'
+                className='flex items-end bg-[#3aac86] overflow-hidden rounded-[130px] md:last:hidden lg:last:block'
                 key={product.productId}
               >
                 <img src={product.image} alt={product.title} />
