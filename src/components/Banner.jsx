8f2026f9b1fdf17622c4fe8364824a207f1fe6a6
standjeong <andame9999@gmail.com>
2024-10-09 14:37:53 +0900

    style : banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ShapeDivider from './ShapeDivider';
 
 export default function Banner() {
   return (
@@ -10,6 +11,7 @@ export default function Banner() {
           alt='banner1'
         />
       </div>
+      <ShapeDivider />
     </section>
   );
 }
