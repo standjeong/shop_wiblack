7b830a0773a35b6c8838c07341f649ed98262ff2
standjeong <andame9999@gmail.com>
2024-10-10 20:07:12 +0900

    feat : total payment amount display in cart page

--- src/components/CartPriceInfo.jsx
+++ src/components/CartPriceInfo.jsx
@@ -6,7 +6,7 @@ export default function CartPriceInfo({ text, price, highlight }) {
   return (
     <div className={`flex justify-between pb-1 ${highlight ? EMPHASIS : ''}`}>
       <p>{text}</p>
-      <p>₩{price}</p>
+      <p>₩{price && price.toLocaleString('kr-KR')}</p>
     </div>
   );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,12 +5,18 @@ import CartPriceInfo from '../components/CartPriceInfo';
 import { getCart } from '../api/firebase';
 import { useAuthContext } from '../context/AuthContext';
 
+const SHIPPING_COST = 3000;
+
 export default function Cart() {
   const { uid } = useAuthContext();
   const { data: cart, isLoading } = useQuery({
     queryKey: ['cart', uid],
     queryFn: () => getCart(uid),
   });
+
+  const totalProductPrice =
+    cart &&
+    cart.reduce((acc, current) => acc + current.price * current.quantity, 0);
 
   return (
     <section className='flex flex-col items-center mb-16 md:mb-0'>
@@ -32,12 +38,16 @@ export default function Cart() {
         </article>
 
         <aside className='md:w-1/3'>
-          <div className='bg-gray-300 p-4 text-lg mb-2'>
-            <CartPriceInfo text='총 상품금액' price='' />
-            <CartPriceInfo text='배송비' price='' />
-            <CartPriceInfo text='총 결제금액' price='' highlight />
+          <div className='bg-slate-200 p-4 text-lg mb-2 rounded-md'>
+            <CartPriceInfo text='총 상품금액' price={totalProductPrice} />
+            <CartPriceInfo text='배송비' price={SHIPPING_COST} />
+            <CartPriceInfo
+              text='총 결제금액'
+              price={totalProductPrice + SHIPPING_COST}
+              highlight
+            />
           </div>
-          <button className='bg-black text-white w-full py-2 font-semibold text-lg'>
+          <button className='bg-black text-white w-full py-2 font-semibold text-lg rounded-md'>
             주문하기
           </button>
         </aside>
