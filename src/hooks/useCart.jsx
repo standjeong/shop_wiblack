f4c8254561e12d38e5e928a3ac4027665b5a58c1
standjeong <andame9999@gmail.com>
2024-10-11 19:20:26 +0900

    feat : enable cart functionality for non-logged-in users

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,28 +1,39 @@
 import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 import { useAuthContext } from '../context/AuthContext';
 import { addOrUpdateToCart, deleteFromCart, getCart } from '../api/firebase';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function useCart() {
-  const { uid } = useAuthContext();
+  const { user, uid } = useAuthContext();
+
+  const tempId = user === false ? getTempId() : null;
+  function getTempId() {
+    if (!uid && !localStorage.getItem('userId')) {
+      const newId = 'temp' + uuidv4();
+      localStorage.setItem('userId', newId);
+      return newId;
+    }
+    return localStorage.getItem('userId');
+  }
 
   const cartQuery = useQuery({
-    queryKey: ['cart'],
-    queryFn: () => getCart(uid),
+    queryKey: ['cart', uid ?? tempId],
+    queryFn: () => getCart(uid ?? tempId),
   });
 
   const queryClient = useQueryClient();
 
   const addOrUpdateToCartMutation = useMutation({
-    mutationFn: (product) => addOrUpdateToCart(product, uid),
+    mutationFn: (product) => addOrUpdateToCart(product, uid ?? tempId),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['cart'] });
+      queryClient.invalidateQueries({ queryKey: ['cart', uid ?? tempId] });
     },
   });
 
   const deleteFromCartMutation = useMutation({
-    mutationFn: (orderId) => deleteFromCart(uid, orderId),
+    mutationFn: (orderId) => deleteFromCart(uid ?? tempId, orderId),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['cart'] });
+      queryClient.invalidateQueries({ queryKey: ['cart', uid ?? tempId] });
     },
   });
 
